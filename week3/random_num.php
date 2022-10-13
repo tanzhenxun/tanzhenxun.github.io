3b1517e9b23bb94193098b600dbcfaca91dd7923
TAN ZHEN XUN <tanzhenxun1118@e.newera.edu.my>
2022-10-13 15:26:24 +0800

    week3

---
+++ week3/random_num.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>php</title>
+    <style>
+        .colour {
+            color: red;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php
+        echo "<p class=\"colour\">hello</p>";
+    ?>
+
+    <h1>Tan Zhen Xun </h1>
+
+    <?php
+        echo date("dS F Y ");
+        $color = "red";
+        echo "<br>My car is " . $color . "<br>"
+    ?>
+    
+
+</body>
+
+</html>
